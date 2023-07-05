f0f4e1817b868078b71707c7e304542f3bf97e07
jaythestampede <jameswboyd32@gmail.com>
2023-07-05 15:23:50 -0400

    band name generator (day1 assignment)

---
+++ basics/100DaysOfPython/band-name-generator.py
@@ -0,0 +1,8 @@
+print('Hello ' + input('What is your name?\n'))
+
+city = input('What city did you grow up in?\n')
+
+pet = input('What is the name of an animal?\n')
+
+print("Your band name could be " + city + " " + pet)
+
